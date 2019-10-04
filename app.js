1f07abb338be71e269f7a83fd91f3172fc8798e9
justyna_zieniewicz <zieniewicz.justyna@gmail.com>
2019-10-04 11:33:20 +0200

    podzial aplikacji na moduly

---
+++ app.js
@@ -0,0 +1,21 @@
+var dataController = (function() {
+    // modul odpowiedzialny za logike dzialania
+    return {
+
+    }
+
+})();
+
+
+var UIController = (function() {
+    // modul odpowiedzialny za pobieranie danych od uzytkownika i ich wysweitlanie
+
+})();
+
+
+var controller = (function(dataCtrl, UICtrl) {
+    // glowny modul, bedacy lacznikiem miedzy dwoma pozostalymi modulami
+
+
+
+})(dataController, UIController);
