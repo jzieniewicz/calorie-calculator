3ec37677a43f79607ae6af28a909f1716997659e
justyna_zieniewicz <zieniewicz.justyna@gmail.com>
2019-10-04 12:27:11 +0200

    dodanie akcji do przycisku dodaj

--- app.js
+++ app.js
@@ -16,8 +16,21 @@ var UIController = (function() {
 // glowny modul, bedacy lacznikiem miedzy dwoma pozostalymi modulami
 var controller = (function(dataCtrl, UICtrl) {
 
-    document.querySelector('.add-button').addEventListener('click', function(){
-        console.log('aaa')
+    var ctrlAddItem = function(){
+        // 1. zabierz dane z wypelnionych pól
+        // 2. dodaj element do dataControllera
+        // 3. dodaj element do UI
+        // 4. przelicz zapotrzebowanie kaloryczne
+        // 5. wyswietl zapotzrebowania
+        console.log('dziala');
+    }
+    document.querySelector('.add-button').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event){
+        if(event.keyCode === 13 || event.which === 13) {
+            event.preventDefault(); //zapobiega wywołaniu przez enter kliknięcia
+            ctrlAddItem();
+        }
     });
 
 })(dataController, UIController);
