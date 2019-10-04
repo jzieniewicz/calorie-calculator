4bf01faf4ff6ea0168b3fafec4b0be82db589961
justyna_zieniewicz <zieniewicz.justyna@gmail.com>
2019-10-04 11:58:39 +0200

    małe poprawki w kodzie

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
+// modul odpowiedzialny za logike dzialania
 var dataController = (function() {
-    // modul odpowiedzialny za logike dzialania
+    
     return {
 
     }
 
 })();
 
-
+// modul odpowiedzialny za pobieranie danych od uzytkownika i ich wysweitlanie
 var UIController = (function() {
-    // modul odpowiedzialny za pobieranie danych od uzytkownika i ich wysweitlanie
+    
 
 })();
 
-
+// glowny modul, bedacy lacznikiem miedzy dwoma pozostalymi modulami
 var controller = (function(dataCtrl, UICtrl) {
-    // glowny modul, bedacy lacznikiem miedzy dwoma pozostalymi modulami
 
-
+    document.querySelector('.add-button').addEventListener('click', function(){
+        console.log('aaa')
+    });
 
 })(dataController, UIController);
